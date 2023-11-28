dfa62572885fa4c3cb497647a966ec64cac2771b
FaresJguerim <faresjguerim7@gmail.com~>
2023-11-28 23:52:05 +0100

    added new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/home';
+import Search from './Pages/Search/search';
+import Favorite from './Pages/Favorite/favorite';
+import Register from './Pages/Register/register';
+import Login from './Pages/Login/login';
+import Products from './Pages/Products/products';
+import ProductDetails from './Pages/Productdetails/productDetails';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Pages/Productdetails/productDetails.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductDetails: React.FC = () => {
+  return <h1>Product Details Page</h1>;
+};
+
+export default ProductDetails;
